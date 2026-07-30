8662c0bf051fd28acfd09298cca9c39ece4b8ff2
Clement <duvalrenouveau@gmail.com>
2026-07-30 02:00:46 +0200

    Delete data directory

--- data/traits.js
+++
@@ -1,8 +0,0 @@
-const TRAITS_DB = {
-    "lache": { name: "Lâche", desc: "La peur vous paralyse facilement." },
-    "courageux": { name: "Sang-Froid", desc: "Rien ne vous fait paniquer." },
-    "ampute_bras": { name: "Amputé (Bras)", desc: "A perdu un bras. Force réduite." },
-    "ampute_jambe": { name: "Amputé (Jambe)", desc: "A perdu une jambe. Agilité réduite." },
-    "tueur_gobelin": { name: "Fléau des Gobelins", desc: "Expertise meurtrière contre les gobelins." },
-    "reputation": { name: "Intimidant", desc: "Votre seule présence fait reculer l'ennemi." }
-};
